ba92c7d994ccfb452b55b798ffd594da96288d14
Virag Vajay <virag.vajay@gmail.com>
2020-09-14 22:31:21 +0200

    first commit new theme

--- wp-content/themes/photofocus-child-theme/functions.php
+++ wp-content/themes/photofocus-child-theme/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
@@ -7,6 +8,7 @@ function my_theme_enqueue_styles() {
         array(),  // if the parent theme code has a dependency, copy it to here
         $theme->parent()->get('Version')
     );
+/* the link below is linking to the child theme*/
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
         array( $parenthandle ),
         $theme->get('Version') // this only works if you have Version in the style header
